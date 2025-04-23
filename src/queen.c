0e52a98617a76e25be504a29705af5d0c937beac
samyro14 <dragossamy2@gmail.com>
2025-04-23 23:51:45 +0300

    Updated files

---
+++ src/queen.c
@@ -0,0 +1,12 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <stdbool.h>
+#include "../board.h"
+#include "../moves.h"
+#include <stdio.h>
+#define BOARD_SIZE 8
+
+
+void move_queen(GameState* state, int from_x, int from_y, int to_x, int to_y){
+    
+}
---
+++ src/rook.c
@@ -0,0 +1,11 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <stdbool.h>
+#include "../board.h"
+#include "../moves.h"
+#include <stdio.h>
+#define BOARD_SIZE 8
+
+void move_rook(GameState* state, int from_x, int from_y, int to_x, int to_y){
+    
+}
