80c24613f129b0c97878d5115b050b08208df464
samyro14 <dragossamy2@gmail.com>
2025-05-26 18:44:08 +0300

    Updated files

--- src/pawn.c
+++ src/pawn.c
@@ -1,6 +1,7 @@
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
 #include <stdbool.h>
 #include "../src/board.h"
 #include "../src/moves.h"
@@ -50,6 +51,12 @@ int is_valid_pawn(GameState* state,int from_x, int from_y, int to_x, int to_y){
 
     return -1; // Mutare invalidă
 }
+void promotion(GameState* state, int x, int y){
+    // Promovare la regină
+    Piece promoted_piece = {QUEEN, state->board[y][x].color};
+    state->board[y][x] = promoted_piece;
+    printf("Pawn promoted to Queen at (%d, %d)\n", x, y);
+}
 
 void move_pawn(GameState* state, int from_x, int from_y, int to_x, int to_y){
     if(is_valid_pawn(state, from_x, from_y, to_x, to_y) == -1){
@@ -66,6 +73,13 @@ void move_pawn(GameState* state, int from_x, int from_y, int to_x, int to_y){
             printf("Invalid move! King is in check!\n");
             return;
         }
+        // Verificare promovare
+        int promotion_row = state->board[to_y][to_x].color == WHITE ? 0 : 7;
+        if (to_y == promotion_row) {
+            promotion(state, to_x, to_y);
+        }
+
+
         state->is_white_turn = !state->is_white_turn;
         return;
     }
